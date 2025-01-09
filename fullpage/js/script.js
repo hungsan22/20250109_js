6c1822307626259bf32b5a113fd0cfe5a7d13200
hungsan22 <168067839+hungsan22@users.noreply.github.com>
2025-01-09 15:17:18 +0900

    Add files via upload

---
+++ fullpage/js/script.js
@@ -0,0 +1,17 @@
+$("#fullpage").fullpage({
+    autoScrolling: true,
+    navigaton: true,
+    anchors: [
+        "furstPage",
+        "secondPage",
+        "thirdPage",
+        "fourthPage",
+    ],
+    sectionsColor: [
+        "#ff5f44",
+        "#0798ec",
+        "#fc6c7c",
+        "#fec401",
+    ],
+    responsiveWidth: 900,
+});
